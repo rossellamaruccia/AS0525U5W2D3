35ff675e6667e632c519873fe350d4384317a0eb
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-02-11 17:26:05 +0100

    sets pages for authors

--- src/main/java/com/example/AS0525U5W2D3/Controllers/AuthorController.java
+++ src/main/java/com/example/AS0525U5W2D3/Controllers/AuthorController.java
@@ -4,10 +4,10 @@ import com.example.AS0525U5W2D3.Entities.Author;
 import com.example.AS0525U5W2D3.Payloads.NewAuthorPayload;
 import com.example.AS0525U5W2D3.Services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,9 +22,14 @@ public class AuthorController {
     }
 
     @GetMapping
-    public List<Author> findAll() {
-        return this.authorService.findAll();
+    public Page<Author> findAll(@RequestParam(defaultValue = "0") int page,
+                                @RequestParam(defaultValue = "10") int size,
+                                @RequestParam(defaultValue = "surname") String orderBy,
+                                @RequestParam(defaultValue = "asc") String sortCriteria) {
+
+        return this.authorService.findAll(page, size, orderBy, sortCriteria);
     }
+
 
     @GetMapping("/{authorId}")
     public Author getAuthorById(@PathVariable UUID authorId) {
--- src/main/java/com/example/AS0525U5W2D3/Services/AuthorService.java
+++ src/main/java/com/example/AS0525U5W2D3/Services/AuthorService.java
@@ -5,9 +5,12 @@ import com.example.AS0525U5W2D3.Payloads.NewAuthorPayload;
 import com.example.AS0525U5W2D3.Repositories.AuthorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -34,8 +37,12 @@ public class AuthorService {
         return newAuthor;
     }
 
-    public List<Author> findAll() {
-        return this.authorRepository.findAll();
+    public Page<Author> findAll(int page, int size, String orderBy, String sortCriteria) {
+        if (size > 100 || size < 0) size = 10;
+        if (page < 0) page = 0;
+        Pageable pageable = PageRequest.of(page, size,
+                sortCriteria.equals("desc") ? Sort.by(orderBy).descending() : Sort.by(orderBy));
+        return this.authorRepository.findAll(pageable);
     }
 
     public Author findById(UUID authorId) {
